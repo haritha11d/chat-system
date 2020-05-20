0dd3d2f79f8efd106ac2a6fb4b888567c5944a4e
haritha11d <rahi17@student.bth.se>
2020-05-21 00:29:32 +0200

    Add code block for protocol part 2

--- chatclient.py
+++ chatclient.py
@@ -23,11 +23,20 @@ try:
     print("connected to server at address " + ip + ":" + str(port) + '\n')
 except:
     print("Not able to connect to server at address " + ip + ":" + str(port) + '\n' + "Exiting :)")
-
+    sys.exit(1)
 # protocol part 1.2 receiving Hello <VERSION> from server
 try:
     hello = client.recv(1024).decode('ascii')
     print(hello)
 except:
     print("Not able to receive Hello <VERSION> msg from the server at address " + ip + ":" + str(port) + '\n' + "Exiting :)")
+    sys.exit(1)
 
+# protocol part 2 sending NICK <nick> and receiving OK/ERR <text> as a response from the server
+nick_load = "NICK " + nick
+client.sendall((nick_load).encode('ascii'))
+response = client.recv(1024).decode('ascii')
+if response == 'Error' or response == 'ERROR':
+    print(response)
+    sys.exit(1)
+print(response)
