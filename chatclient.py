67c588eef1ccbf28467cb3ec28152a20d14e23ef
haritha11d <rahi17@student.bth.se>
2020-05-21 00:18:36 +0200

    Add try connection code block

--- chatclient.py
+++ chatclient.py
@@ -8,5 +8,18 @@ import socket
 if len(sys.argv) != 3:
     print("Usage: ./chat-client <server-ip>:<portnum> nick")
     sys.exit(1)
-else:
-    print("All arguments given correct!")
+
+# Split the arguments into ip, port, nick
+
+args = str(sys.argv[1]).split(':')
+ip = str(args[0])
+port = int(args[1])
+nick = str(sys.argv[2])
+
+# try to connect to the server
+client = socket.socket()
+try:
+    client.connect((ip,port))
+    print("connected to server at address " + ip + ":" + str(port) + '\n')
+except:
+    print("Not able to connect to server at address " + ip + ":" + str(port) + '\n' + "Exiting :)")
